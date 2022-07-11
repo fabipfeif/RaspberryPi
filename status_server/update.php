1657ba176d65eb573f3a0f60ae97d027beba069b
fabipfeif <fabianpfeiffer00mail.com>
2022-07-11 16:06:43 +0200

    time

--- status_server/update.php
+++ status_server/update.php
@@ -2,13 +2,14 @@
 
 header('Content-Type: application/json');
 
-
+date_default_timezone_set("Europe/Paris");
 $filename = "/sys/class/thermal/thermal_zone0/temp";
 $fp = fopen($filename, "r");
 $contents = fread($fp, filesize($filename));
 $data = [
 
-	'Cputemp' => $contents
+	'Cputemp' => $contents,
+	'time' => date("h:i:sa"),
 ];
 fclose($fp);
 echo json_encode($data);
